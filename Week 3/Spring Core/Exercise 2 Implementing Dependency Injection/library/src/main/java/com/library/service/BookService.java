10d1ae5b06a07027d0786eeb64c0ad9e8db86aa6
Ishanya Kukade <ishanyak12@gmail.com>
2025-07-03 07:26:04 +0530

    Exercise 2: Implementing Dependency Injection

---
+++ Week 3/Spring Core/Exercise 2 Implementing Dependency Injection/library/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,26 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public BookService() {
+    }
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public String findBookId(int id) {
+        return "Book id: " + bookRepository.getId(id);
+    }
+}
