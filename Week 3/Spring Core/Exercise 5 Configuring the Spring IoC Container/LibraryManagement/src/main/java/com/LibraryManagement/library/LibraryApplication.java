c7d095952d6fd5f96ee6e903268de60cc5bc7d88
Ishanya Kukade <ishanyak12@gmail.com>
2025-07-03 23:49:56 +0530

    Corrections

---
+++ Week 3/Spring Core/Exercise 5 Configuring the Spring IoC Container/LibraryManagement/src/main/java/com/LibraryManagement/library/LibraryApplication.java
@@ -0,0 +1,18 @@
+package com.LibraryManagement.library;
+
+import com.LibraryManagement.library.service.BookService;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService = context.getBean("bookService", BookService.class);
+
+        bookService.addBook("ABC");
+    }
+}
