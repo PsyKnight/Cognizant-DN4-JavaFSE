c3bd1eb599741245e19cd1a886a4fe1696601a21
Ishanya Kukade <ishanyak12@gmail.com>
2025-07-19 22:32:00 +0530

    7. ReactJS-HOL

---
+++ Week 6/ReactJS-HOL/7_shoppingapp/src/App.js
@@ -0,0 +1,12 @@
+import OnlineShopping from "./components/OnlineShopping";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <OnlineShopping />;
+    </div>
+  );
+};
+
+export default App;
