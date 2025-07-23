4d60af2838211f433ca42e55033b4160d71336ba
Ishanya Kukade <ishanyak12@gmail.com>
2025-07-24 00:19:15 +0530

    Edit EmployeeList.js

--- Week 7/ReactJS-HOL/14_employeesapp/src/EmployeesList.js
+++ Week 7/ReactJS-HOL/14_employeesapp/src/EmployeesList.js
@@ -1,12 +1,11 @@
 import EmployeeCard from "./EmployeeCard";
 import { useTheme } from "./components/ThemeContext";
 function EmployeesList(props) {
-  const { theme } = useTheme();
   return (
     <div>
       <h1>Employees List</h1>
       {props.employees.map((employee) => (
-        <EmployeeCard theme={theme} employee={employee} key={employee.id} />
+        <EmployeeCard employee={employee} key={employee.id} />
       ))}
     </div>
   );
